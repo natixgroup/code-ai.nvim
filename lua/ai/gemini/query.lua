64854eac9367021f01137d54fa2d69be206e5fa4
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2024-07-30 23:45:31 +0300

    economize some query

--- lua/ai/gemini/query.lua
+++ lua/ai/gemini/query.lua
@@ -69,13 +69,6 @@ function query.askHeavy(instruction, prompt, opts, api_key, agent_host)
   end
   table.insert(contents, {contents = {role = 'model', parts = {{text = "Then what do you want me to do with all that information?"}}}})
   table.insert(contents, {contents = {role = 'user', parts = {{text = prompt}}}})
-  table.insert(contents, {safetySettings = {
-    { category = 'HARM_CATEGORY_SEXUALLY_EXPLICIT', threshold = 'BLOCK_NONE' }, 
-    { category = 'HARM_CATEGORY_HATE_SPEECH', threshold = 'BLOCK_NONE' },
-    { category = 'HARM_CATEGORY_HARASSMENT', threshold = 'BLOCK_NONE' },
-    { category = 'HARM_CATEGORY_DANGEROUS_CONTENT', threshold = 'BLOCK_NONE' }}
-  })
-  table.insert(contents, {generationConfig = {temperature = 0.2, topP = 0.5}})
   table.insert(contents, {})
   for i, content in ipairs(contents) do
     local body = json.encode(content)
